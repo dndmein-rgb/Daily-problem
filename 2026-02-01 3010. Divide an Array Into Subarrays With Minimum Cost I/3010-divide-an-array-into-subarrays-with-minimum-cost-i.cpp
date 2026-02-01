4808a6fffc7222a9164abae99111c62a6f741fb3
Divyanshu Rathore <dnd.mein@gmail.com>
2026-02-01 23:03:14 +0530

    Create 3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp

---
+++ 2026-02-01 3010. Divide an Array Into Subarrays With Minimum Cost I/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        int second = INT_MAX, third = INT_MAX;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < second) {
+                third = second;
+                second = nums[i];
+            } else if (nums[i] < third) {
+                third = nums[i];
+            }
+        }
+        return nums[0] + second + third;
+    }
+};
