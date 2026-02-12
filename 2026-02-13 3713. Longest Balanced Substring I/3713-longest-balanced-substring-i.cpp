2d9b9cbd69b90dc084639cdc98e86713e5347cb6
Divyanshu Rathore <dnd.mein@gmail.com>
2026-02-12 22:34:33 +0530

    Create 3713-longest-balanced-substring-i.cpp

---
+++ 2026-02-13 3713. Longest Balanced Substring I/3713-longest-balanced-substring-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int longestBalanced(string s) {
+        int maxi = 0;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            vector<int> freq(26, 0);
+
+            for (int j = i; j < n; j++) {
+                freq[s[j] - 'a']++;
+
+                int target = freq[s[j] - 'a'];
+                bool flag = true;
+
+                for (int k = 0; k < 26; k++) {
+                    if (freq[k] > 0 && freq[k] != target) {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag) {
+                    maxi = max(maxi, j - i + 1);
+                }
+            }
+        }
+        return maxi;
+    }
+};
